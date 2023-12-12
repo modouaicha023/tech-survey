c270fc9e25ab29562bab44b7d4515030c48c3882
Modou Aicha Diop <modouaichadiop@gmail.com>
2023-12-12 20:55:29 +0000

    add SurveyItem and firebase config

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,38 @@
+import SurveyItem from '@/components/SurveyItem'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hi
+
+      <div className="flex p-6">
+        <span className="text-2xl font-bold bg-gradient-to-r from-green-400 to-yellow-500 bg-clip-text text-transparent self-end">Dev</span>
+        <span className="text-5xl font-extrabold bg-gradient-to-r from-red-600 to-amber-300 bg-clip-text text-transparent">Survey</span>
+      </div>
+
+      <div className=''>
+        <SurveyItem />
+      </div>
+
+      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+        <a
+          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Docs{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Find in-depth information about Next.js features and API.
+          </p>
+        </a>
+      </div>
+
     </main>
   )
 }
---
+++ src/utils/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.FIREBASE_API_KEY,
+  authDomain: "dev-survey-35035.firebaseapp.com",
+  databaseURL: "https://dev-survey-35035-default-rtdb.firebaseio.com",
+  projectId: "dev-survey-35035",
+  storageBucket: "dev-survey-35035.appspot.com",
+  messagingSenderId: "1026585456398",
+  appId: "1:1026585456398:web:98b4d8feea922009acb15f",
+  measurementId: "G-XFPQ9TCCLC"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
